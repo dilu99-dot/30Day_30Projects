75346c89d67f6d33849014d889150aca29ec4d56
dilupa Karunathilaka <dulipakarunathilaka@gmail.com>
2025-05-19 21:17:43 +0530

    5 - Project_Txttospeech

---
+++ Text to Speech/script.js
@@ -0,0 +1,21 @@
+let speach = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector('select');
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speach.voice = voices[0];
+  voices.forEach((voice,i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+
+};
+
+voiceSelect.addEventListener('change', () => {
+  speach.voice = voices[voiceSelect.value];
+});
+
+
+document.querySelector('button').addEventListener('click', () => {
+  speach.text = document.querySelector('textarea').value;
+  window.speechSynthesis.speak(speach);
+});
